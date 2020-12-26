5f543ce4c4ba68533ff0497849b6ddeee44c2283
Dhinesh <64545984+BalaDhinesh@users.noreply.github.com>
2020-12-26 10:33:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ var createConnection = function (sourcePort, targetPort) {
   return c;
 };
 
-document.getElementById("json").style.display = "none";
+// document.getElementById("json").style.display = "none";
 
 function displayJSON(canvas) {
   var writer = new draw2d.io.json.Writer();
